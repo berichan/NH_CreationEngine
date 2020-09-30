7b9d723497f592d8e77323aa8cf579e52847f864
berichan <berichandev@gmail.com>
2020-09-30 19:40:08 +0100

    1.5.0

--- NHCE_src/NH_CreationEngine/Program.cs
+++ NHCE_src/NH_CreationEngine/Program.cs
@@ -1,30 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.IO;
-using System.Linq;
+using NHSE.Core;
 using MsbtLite;
+using System.IO;
 
 namespace NH_CreationEngine
 {
     class Program
     {
         //edit this
-        public const string dumpPath = @"D:\Switch\ACNH\Unpackv2\patched_acnh_1_4_0\"; // should have your romBCSVfs and romSARCfs in it. Use https://github.com/berichan/ACNH_Dumper
+        public const string dumpPath = @"D:\Switch\ACNH\Unpackv2\patched_acnh_1_5_0\"; // should have your romBCSVfs and romSARCfs in it. Use https://github.com/berichan/ACNH_Dumper
 
         // Requires you to dump your own menu icon map using sysbot on your switch using the other project in this solution (NH_Sysbot_Tools) and move the berimap file it creates into /NH_CreationEngine/Dumpfiles/mainIcon.berimap 
         // once you get your items filtered you may use my fork of switch toolbox to batch convert textures, this preserves filenames (untick both top options, one of them is creating new folder which you don't need) https://github.com/berichan/Switch-Toolbox
         static void Main(string[] args)
         {
             //ShowNMTFile();
-            //Util.SearchAllBCSVFor("Sead");
+            //Util.SearchAllBCSVFor("Pete");
             //Util.PrintBigFolders(PathHelper.ModelPath);
 
             //SpriteCreationEngine.GenerateMenuIconList();
-            //SpriteParser.DumpImagesToSingleFile(@"D:\Switch\ACNH\Unpackv2\patched_acnh_1_4_0\Output\Sprites\Spritesbfres_menu\processed", @"D:\Switch\ACNH\Unpackv2\patched_acnh_1_4_0\Output\Sprites\imagedump_menu.dmp");
+            //SpriteParser.DumpImagesToSingleFile(@"D:\Switch\ACNH\Unpackv2\patched_acnh_1_5_0\Output\Sprites\Spritesbfres_menu\output", @"D:\Switch\ACNH\Unpackv2\patched_acnh_1_5_0\Output\Sprites\imagedump_menu.dmp");
 
             //SpriteCreationEngine.DoItemSearch();
-            //SpriteParser.DumpImagesToSingleFile(@"D:\Switch\ACNH\Unpackv2\Images_Master", @"D:\Switch\ACNH\Unpackv2\patched_acnh_1_4_0\Output\Sprites\imagedump.dmp");
+            //SpriteParser.DumpImagesToSingleFile(@"D:\Switch\ACNH\Unpackv2\Images_Master", @"D:\Switch\ACNH\Unpackv2\patched_acnh_1_5_0\Output\Sprites\imagedump.dmp");
 
             //ModelCreationEngine.DoItemSearchUnitIcon();
 
@@ -34,7 +34,7 @@ namespace NH_CreationEngine
             //var watch = System.Diagnostics.Stopwatch.StartNew();
             //BCSVHelper.RedumpBCSV();
             //BCSVHelper.CreateMenuIconParam(@"D:\Switch\ACNH\Unpackv2\patched_acnh_1_4_0\romBCSVfs\Bcsv\text\menuicon.txt");
-            DoEverything();
+            //DoEverything();
             //watch.Stop();
             //Console.WriteLine(string.Format("All files written in {0}ms", watch.ElapsedMilliseconds));
         }
@@ -57,6 +57,26 @@ namespace NH_CreationEngine
             ClassCreationEngine.CreateRemakeUtil();
             ClassCreationEngine.CreateRecipeUtil();
             ClassCreationEngine.CreateMenuIcon();
+        }
+
+        static void ConvertVillagers(string inPath, string outPath)
+        {
+            if (!Directory.Exists(outPath))
+                Directory.CreateDirectory(outPath);
+
+            DirectoryInfo di = new DirectoryInfo(inPath);
+            FileInfo[] files = di.GetFiles();
+
+            foreach (var file in files)
+            {
+                if (file.Name.EndsWith("V.bytes"))
+                {
+                    var villager = File.ReadAllBytes(file.FullName);
+                    Villager1 v1 = new Villager1(villager);
+                    Villager2 v2 = new Villager2(VillagerConverter.Convert12(villager));
+                    File.WriteAllBytes(Path.Combine(outPath, file.Name), v2.Data);
+                }
+            }
         }
 
         // Example functions
